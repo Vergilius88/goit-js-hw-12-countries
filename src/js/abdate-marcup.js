b2041ff7d86dd3dc1b9d4314effbb08a4e599392
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-10-18 16:46:44 +0300

    fixed markup

--- src/js/abdate-marcup.js
+++ src/js/abdate-marcup.js
@@ -2,7 +2,6 @@ import { alert, notice, info, success, error } from '@pnotify/core';
 import countriesTemplateItem from '../templates/templates-countries-item.hbs';
 import countriesTemplateList from '../templates/templates-countries-list.hbs';
 import ref from './refs.js';
-
 function markupSelection(item) {
   if (!item) {
     ref.listCountriesRef.innerHTML = '';
@@ -12,6 +11,7 @@ function markupSelection(item) {
       title: 'Oh No!',
       text: 'Too many matches found.Please enter a more specific query',
     });
+    ref.listCountriesRef.innerHTML = '';
   } else if (item.length >= 2 && item.length <= 10) {
     ref.listCountriesRef.innerHTML = countriesTemplateList(item);
   } else {
